88c2f405c996fc249dc9a914963d029cc33f419e
hanbit-kang <fchopinof99@gmail.com>
2021-02-26 18:48:39 +0900

    가장 긴 바이토닉 부분 수열

--- 가장 긴 바이토닉 부분 수열.py
+++ 가장 긴 바이토닉 부분 수열.py
@@ -1,36 +1,19 @@
-def solution(List):
-    for i in range(0, len(List)-2):
-        if IsSubListsBitonic(List, i): return len(List)-i
-    return False
-
-def IsSubListsBitonic(List, n):
-    prev=getSubLists(List)
-    for i in range(n-1):
-        cur=[]
-        for j in range(len(prev)):
-            cur+=getSubLists(prev[j])
-        prev=cur
-    for i in range(len(prev)):
-        if IsBitonic(prev[i]): return True
-    return False
-
-def getSubLists(List):
-    RET = []
-    for i in range(len(List)):
-        RET.append(List[:i]+List[i+1:])
-    return RET
-
-def IsBitonic(nums):
-    for i in range(len(nums)-1):
-        if nums[i]>nums[i+1]:
-            for j in range(i+1, len(nums)-1):
-                if nums[j]<nums[j+1]: return False
-            return True
-    return True
-
 N = int(input())
-lst = str(input())
-lst = lst.split()
-for i in range(len(lst)):
-    lst[i]=int(lst[i])
-print(solution(lst))
+A = list(map(int, input().split()))
+
+dpAscend = [0]*N
+for i in range(N):
+    for j in range(i):
+        if A[i]>A[j] and dpAscend[i]<=dpAscend[j]:
+            dpAscend[i] = dpAscend[j]+1
+
+dpDescend = [0]*N
+for i in range(N-1, -1, -1):
+    for j in range(N-1, i, -1):
+        if A[i]>A[j] and dpDescend[i]<=dpDescend[j]:
+            dpDescend[i] = dpDescend[j]+1
+
+RET = 0
+for i in range(N):
+    RET = max(RET, dpAscend[i]+dpDescend[i]+1)
+print(RET)
