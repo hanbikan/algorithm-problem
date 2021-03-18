c8a36a063c6f288b1d008744b3ea093119526b67
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-03-18 15:49:03 +0900

    210318

--- 파일 합치기.py
+++ 파일 합치기.py
@@ -1,16 +1,19 @@
-def solution(k, datum):
-    cases=[ [[datum, 0]] ]
-    for i in range(0, k):
-        newCases=[]
-        for c in range(len(cases[i])): #case trace
-            for j in range(len(cases[i][c][0])-1): #파일 용량들 trace
-                new_datum=cases[i][c][0][:j]
-                new_datum.append(cases[i][c][0][j]+cases[i][c][0][j+1])
-                if len(cases[i][c][0])-1 >= j+2: new_datum+=cases[i][c][0][j+2:]
-                newCases.append([new_datum,cases[i][c][1]+cases[i][c][0][j]+cases[i][c][0][j+1]])
+import sys
+input = sys.stdin.readline
 
-        cases.append(newCases)
-    print(cases[i])
+T = int(input())
+for _ in range(T):
+    K = int(input())
+    heap = []
+    nums = [0]+list(map(int, input().split()))
 
-#solution(4, [40, 30, 30, 50])
-solution(15, [1,21,3,4,5,35,5,4,3,5,98,21,14,17,32])
+    sums = [0]*(K+1)
+    for i in range(1, K+1):
+        sums[i] = sums[i-1]+nums[i]
+
+    dp = [[0]*(K+1) for _ in range(K+1)]
+    for i in range(2, K+1):  # 길이
+        for j in range(1, K+2-i):  # 시작지점..K-1~0
+            dp[j][j+i-1] = min([dp[j][j+k]+dp[j+k+1][j+i-1]
+                                for k in range(i-1)]) + (sums[j+i-1]-sums[j-1])
+    print(dp[1][K])
