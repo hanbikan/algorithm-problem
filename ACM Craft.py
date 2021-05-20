0dff01bd40429501aeca0294844208c1afc01d3f
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-05-20 18:47:07 +0900

    20210520 골드3

--- ACM Craft.py
+++ ACM Craft.py
@@ -1,45 +1,69 @@
 import sys
-read = sys.stdin.readline
+input = sys.stdin.readline
 
-def getMaxTimeFromZ2(idx):
-    RET = 0
-    if Z2.get(idx):
-        times=[]
-        for i in range(len(Z2[idx])):
-            times.append(D[Z2[idx][i]])
-        RET += max(times)
-    return RET
 
-T=int(read())
-for I in range(T):
-    N, K = map(int, read().strip().split())
-    dp = [0]*N
-    D=list(map(int, read().strip().split()))
-    X, Y = [], []
-    Z1, Z2 = {}, {}
-    for J in range(K):
-        x, y = map(int, read().strip().split())
-        X.append(x)
-        Y.append(y)
-        if not Z1.get(x-1):
-            Z1[x-1]=[y-1]
-        else:
-            Z1[x-1].append(y-1)
-        if not Z2.get(y-1):
-            Z2[y-1]=[x-1]
-        else:
-            Z2[y-1].append(x-1)
-    for i in range(N):
-        if not Z2.get(i):
-            todo = [i]
-            break
-    W=int(read())
-    while todo!=[]:
-        print(todo)
-        nexttodo = []
-        for i in range(len(todo)):
-            dp[i] += getMaxTimeFromZ2(todo[i]) + D[i]
-            if Z1.get(todo[i]):
-                nexttodo += Z1[todo[i]]
-        todo = nexttodo
-    print(dp)
+def dfs(building, depth):
+    global buildingsOnDepth
+
+    if buildingsOnDepth.get(depth):
+        if building in buildingsOnDepth[depth]:
+            return
+
+        buildingsOnDepth[depth].add(building)
+    else:
+        buildingsOnDepth[depth] = set([building])
+
+    # 탈출
+    if building == targetBuilding:
+        return
+
+    for next in nextBuildings[building]:
+        dfs(next, depth+1)
+
+
+def searchBuilding(building):
+    global costsToBuild
+
+    # 탈출
+    if building == targetBuilding:
+        return
+
+    for next in nextBuildings[building]:
+        costsToBuild[next] = max(
+            costsToBuild[next], costsToBuild[building] + costs[next])
+
+
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+
+    costs = [0]
+    costs += list(map(int, input().split()))
+
+    nextBuildings = {i: [] for i in range(N+1)}
+    requiredBuildings = {i: [] for i in range(N+1)}
+
+    for _ in range(K):
+        cur, next = map(int, input().split())
+        nextBuildings[cur].append(next)
+        requiredBuildings[next].append(cur)
+
+    # 헤더 빌딩(0번)에, 처음부터 지을 수 있는 건물들 연결
+    for b in range(1, N+1):
+        if requiredBuildings[b] == []:
+            nextBuildings[0].append(b)
+            requiredBuildings[b].append(0)
+
+    targetBuilding = int(input())
+
+    # 깊이 별로 빌딩들을 나눔
+    buildingsOnDepth = {}
+    dfs(0, 0)
+
+    # 깊이에 따른 탐색
+    costsToBuild = [0 for _ in range(N+1)]
+    for buildings in buildingsOnDepth.values():
+        for b in buildings:
+            searchBuilding(b)
+
+    print(costsToBuild[targetBuilding])
--- 이분 그래프.py
+++ 이분 그래프.py
@@ -26,7 +26,6 @@ for _ in range(K):
     V, E = map(int, input().split())
     graph = {i: [] for i in range(1, V+1)}
 
-    edges = []
     for _ in range(E):
         e1, e2 = map(int, input().split())
         graph[e1].append(e2)
@@ -35,7 +34,7 @@ for _ in range(K):
     # -1: Red, 0: Undefined, 1: Blue
     verticesColor = [0 for _ in range(V+1)]
     isBipartiteGraph = True
-    for v, e in graph.items():
+    for v in graph.keys():
         if verticesColor[v] == 0:
             if setVerticesColor(v) == False:
                 isBipartiteGraph = False
