309e13fdf993bbec9f4edafbf6ddae7b3cc615be
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-06-03 23:54:09 +0900

    보석 도둑: 실패, 아직 그리드를 완벽하게 이해하지 못한 것 같다. 더 많은 연습량이 필요하다. 정말 효율적으로 메모리를 사용하는 직관을 길러야한다.

--- 보석 도둑.py
+++ 보석 도둑.py
@@ -1,29 +1,27 @@
-import math
-def getGreaterEqualIndex(sortedNums, num):
-    left, right = 0, len(sortedNums)-1
-    while left<right+1:
-        mid = (left+right)//2
-        if num<sortedNums[mid]:
-            right = mid-1
-        elif num>sortedNums[mid]:
-            left = mid+1
-        else: return mid
-    return right+1
+import sys
+import heapq
+input = sys.stdin.readline
 
+# 입력
 N, K = map(int, input().split())
-M, V = 0, 1
-Jewelries = []
-for _ in range(N):
-    Jewelries.append(list(map(int, input().split())))
-Jewelries = sorted(Jewelries, key=lambda x:x[V], reverse=True)
-C = []
-for _ in range(K): C.append(int(input()))
-C.sort() # 2 10
 
-RET=0
-for Jewelry in Jewelries:
-    idxMinBag = getGreaterEqualIndex(C, Jewelry[M])
-    if idxMinBag < len(C):
-        del C[idxMinBag]
-        RET+=1
-print(RET)
+jewelries = sorted([list(map(int, input().split())) for _ in range(N)])
+bags = sorted([int(input()) for _ in range(K)])
+
+# 메인 로직
+valueSum = 0  # 출력값
+availableJewelries = []  # bag 이하인 보석들을 저장
+jewelryIndex = 0
+
+for bag in bags:
+    while jewelryIndex <= N-1:
+        if jewelries[jewelryIndex][0] > bag:
+            break
+
+        heapq.heappush(availableJewelries, (-1)*jewelries[jewelryIndex][1])
+        jewelryIndex += 1
+
+    if availableJewelries:
+        valueSum += (-1)*heapq.heappop(availableJewelries)
+
+print(valueSum)
