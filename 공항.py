7777ee2c6facbfaaf48b5f0dd7edb9b8c6182199
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-07-27 11:42:20 +0900

    공항: 성공, 재채점(시간초과)에 대한 코드 수정

--- 공항.py
+++ 공항.py
@@ -1,9 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
 def findParent(x):
     if parent[x] == x:
         return x
+
     p = findParent(parent[x])
     parent[x] = p
-    return parent[x]
+
+    return p
 
 
 def union(x, y):
@@ -15,18 +21,20 @@ def union(x, y):
         parent[x] = y
 
 
-G = int(input())
-P = int(input())
-airplanes = [int(input()) for _ in range(P)]
+if __name__ == '__main__':
+    G = int(input())
+    P = int(input())
 
-parent = {i: i for i in range(G+1)}
+    airplanes = [int(input()) for _ in range(P)]
+    parent = [i for i in range(G+1)]
+    cntDocked = 0
 
-cntDocked = 0
-for airplane in airplanes:
-    x = findParent(airplane)
-    if x == 0:
-        break
-    union(x, x-1)
-    cntDocked += 1
+    for airplane in airplanes:
+        x = findParent(airplane)
 
-print(cntDocked)
+        if x == 0:
+            break
+        union(x, x-1)
+        cntDocked += 1
+
+    print(cntDocked)
