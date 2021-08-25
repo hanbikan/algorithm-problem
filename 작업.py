c79c5104c8e60914a96c9146effbb4ecf7628f4e
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-08-25 12:05:36 +0900

    전깃줄 - 2: 성공, 플레 5

--- 작업.py
+++ 작업.py
@@ -1,24 +1,17 @@
-RET=0
-N=7
-tasks=[[5,0],[1,1,1],[3,1,2],[6,1,1],[1,2,2,4],[8,2,2,4],[4,3,3,5,6]]
-DP = [[] for i in range(N)]
-for i in range(len(tasks)):
-    for j in range(2, len(tasks[i])):
-        DP[tasks[i][j]-1].append(i)
+import sys
+input = sys.stdin.readline
 
-todo=[0]
-while True:
-    print(todo, tasks)
-    if todo==[]: break
-    maxTime=0
-    nextTodo=[]
-    for i in range(len(todo)):
-        if len(todo)==2: maxTime = max(maxTime, tasks[todo[i]][0])
-        for j in range(len(DP[todo[i]])):
-            for k in range(2, len(tasks[DP[todo[i]][j]])):
-                if tasks[DP[todo[i]][j]][k]==todo[i]: del tasks[DP[todo[i]][j]][k]
-            nextTodo.append(DP[todo[i]][j])
-    todo = nextTodo
-    RET+=maxTime
+if __name__ == '__main__':
+    N = int(input())
+    costs = [0]*(N+1)
 
-print(maxTime)
+    for i in range(1, N+1):
+        cur_input = list(map(int, input().split()))
+
+        prev_max_time = 0
+        for j in range(2, 2 + cur_input[1]):
+            prev_max_time = max(prev_max_time, costs[cur_input[j]])
+
+        costs[i] = prev_max_time + cur_input[0]
+
+    print(max(costs))
