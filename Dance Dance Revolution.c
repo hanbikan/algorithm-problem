564dade4cb7bf291714304b20d8e9aeaad40e13e
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-09-14 11:48:54 +0900

    210914

--- Dance Dance Revolution.c
+++ Dance Dance Revolution.c
@@ -1,29 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define CASE 4
+#define MIN(a, b) (a<b?a:b)
+
 SIZE = 100000;
-CASE = 4;
 
 int get_cost(int p, int n) {
 	if (p == n) return 1;
 	else if (p == 0) return 2;
-	else if ((p + n)%2 == 0) return 4;
+	else if ((p + n) % 2 == 0) return 4;
 	else return 3;
 }
 
-int dfs(int* nums, int ***dp, int index, int left, int right) {
+int get_min_strength(int* nums, int*** dp, int index, int left, int right) {
 	if (index >= SIZE) return 0;
-	if ((left != 0 && left == right) || dp[left][right][index] < SIZE * CASE + 1) return dp[left][right][index];
+
+	// 한 발판에 두 발을 두는 것 허용X -> DP에 초기에 매우 큰 값이 들어가므로 이것을 리턴한다.
+	// 이렇게 하면, 이것의 상위 함수에서 min을 통해 걸러낼 수 있다.
+	else if (left != 0 && left == right) return dp[left][right][index];
+
+	// DP를 통한 최적화 
+	else if(dp[left][right][index] < SIZE * CASE + 1) return dp[left][right][index];
 
 	int cost1 = get_cost(left, nums[index]);
 	int cost2 = get_cost(right, nums[index]);
 
-	int res1 = cost1 + dfs(nums, dp, index + 1, nums[index], right);
-	int res2 = cost2 + dfs(nums, dp, index + 1, left, nums[index]);
-	
-	if (res1 > res2)
-		dp[left][right][index] = res2;
-	else
-		dp[left][right][index] = res1;
+	int res1 = cost1 + get_min_strength(nums, dp, index + 1, nums[index], right);
+	int res2 = cost2 + get_min_strength(nums, dp, index + 1, left, nums[index]);
+
+	// 두 개 중 하나를 선택하여 DP값 수정
+	dp[left][right][index] = MIN(res1, res2);
 
 	return dp[left][right][index];
 }
@@ -37,19 +43,19 @@ int solution(int* nums) {
 	int i, j, k;
 
 	// DP 초기화
-	int*** dp = malloc(sizeof(int **) * (CASE+1));
-	for (i = 0; i < CASE+1; i++) {
-		dp[i] = malloc(sizeof(int *) * (CASE + 1));
-		for (j = 0; j < CASE+1; j++) {
+	int*** dp = malloc(sizeof(int**) * (CASE + 1));
+	for (i = 0; i < CASE + 1; i++) {
+		dp[i] = malloc(sizeof(int*) * (CASE + 1));
+		for (j = 0; j < CASE + 1; j++) {
 			dp[i][j] = malloc(sizeof(int) * SIZE);
 			for (k = 0; k < SIZE; k++) {
 				dp[i][j][k] = SIZE * CASE + 1;
-			 }
+			}
 		}
 	}
 
-	// 탐색
-	answer = dfs(nums, dp, 0, 0, 0);
+	// 재귀 호출
+	answer = get_min_strength(nums, dp, 0, 0, 0);
 
 	// 메모리 해제
 	for (i = 0; i < CASE + 1; i++) {
@@ -64,10 +70,10 @@ int solution(int* nums) {
 }
 
 int main() {
-	int* nums = malloc(sizeof(int) * (SIZE+1));
+	int* nums = malloc(sizeof(int) * (SIZE + 1));
 	int i;
 
-	for (i = 0; i < SIZE+1; i++) {
+	for (i = 0; i < SIZE + 1; i++) {
 		scanf("%d", &nums[i]);
 		if (nums[i] == 0) {
 			SIZE = i;
