751c2bc696797bf091b444d4e51a61b048760db0
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-10-08 21:30:35 +0900

    211008

--- 구간 합 구하기.py
+++ 구간 합 구하기.py
@@ -2,82 +2,36 @@ import sys
 input = sys.stdin.readline
 
 
-class Node:
-    def __init__(self):
-        self.start = None
-        self.end = None
-        self.left = None
-        self.right = None
-        self.var = None
+def update_fenwick_tree(tree, index, value):
+    while index < len(tree):
+        tree[index] += value
+        index += (index & -index)
+
+    return tree
 
 
-def initializeSegmentTree(node, start, end):
-    node.start = start
-    node.end = end
+def query_fenwick_tree(tree, index):
+    res = 0
+    while index > 0:
+        res += tree[index]
+        index -= (index & -index)
 
-    if start == end:  # 말단
-        node.var = nums[start]
-    else:
-        # 자식 노드 생성하고 dfs 로직으로 초기화
-        node.left = Node()
-        initializeSegmentTree(node.left, start, (start+end)//2)
-
-        node.right = Node()
-        initializeSegmentTree(node.right, (start+end)//2+1, end)
-
-        # 자식을 합함
-        node.var = node.left.var + node.right.var
+    return res
 
 
-def modifySegmentTree(node):
-    node.var += c-nums[b]  # 세그먼트 트리 값 변경
+if __name__ == '__main__':
+    N, M, K = map(int, input().split())
 
-    if node.start == node.end:
-        nums[b] = c  # nums도 꼭 수정을 해야함
-        return
+    tree = [0]*(N+1)
+    nums = [int(input()) for _ in range(N)]
+    for i in range(N):
+        tree = update_fenwick_tree(tree, i+1, nums[i])
 
-    # b가 해당 자식의 범위에 있을 시 탐색
-    if node.left and node.left.start <= b <= node.left.end:
-        modifySegmentTree(node.left)
-    elif node.right and node.right.start <= b <= node.right.end:
-        modifySegmentTree(node.right)
+    for _ in range(M + K):
+        a, b, c = map(int, input().split())
 
-
-def setSegmentSum(node, start, end):
-    global sum
-
-    if start <= node.start and node.end <= end:
-        # sum 갱신
-        sum += node.var
-
-        # 나뉘어진 새 범위에 대해서 탐색
-        if not (node.start == start and node.end == end):
-            if b <= node.start-1 and node.left and start <= node.left.end:
-                setSegmentSum(node.left, start, node.start-1)
-            if node.end+1 <= c and node.right and node.right.start <= end:
-                setSegmentSum(node.right, node.end+1, end)
-
-    else:
-        if node.left and start <= node.left.end:
-            setSegmentSum(node.left, start, end)
-        if node.right and node.right.start <= end:
-            setSegmentSum(node.right, start, end)
-
-
-N, M, K = map(int, input().split())
-nums = [0]
-for _ in range(N):
-    nums.append(int(input()))
-
-root = Node()
-initializeSegmentTree(root, 1, N)
-
-for _ in range(M + K):
-    a, b, c = map(int, input().split())
-
-    if a == 1:
-        modifySegmentTree(root)
-    elif a == 2:
-        sum = 0
-        setSegmentSum(root, b, c)
-        print(sum)
+        if a == 1:
+            tree = update_fenwick_tree(tree, b, c - nums[b-1])
+            nums[b-1] = c
+        elif a == 2:
+            print(query_fenwick_tree(tree, c) - query_fenwick_tree(tree, b-1))
