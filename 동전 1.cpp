67e28b1c33da24411b0c2061151e84128e290866
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-11-08 21:50:08 +0900

    211108

---
+++ 동전 1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	
+	int i, j;
+	int *coins = (int *)malloc(sizeof(int)*n);
+	for (i = 0; i < n; i++) cin >> coins[i];
+
+	// Solution
+	int dp[10001] = {1};
+
+	for (i = 0; i < n; i++) {
+		int coin = coins[i];
+		for (j = 1; j < k+1; j++) {
+			if (j - coin >= 0) dp[j] += dp[j - coin];
+		}
+	}
+
+	cout << dp[k];
+
+	free(coins);
+
+	return 0;
+}
---
+++ 색종이 만들기.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+bool is_all_same_papers(int** nums, int x1, int y1, int x2, int y2) {
+	int val = nums[x1][y1];	// 비교 대상
+
+	for (int i = x1; i < x2 + 1; i++) {
+		for (int j = y1; j < y2 + 1; j++) {
+			if (nums[i][j] != val) return false;
+		}
+	}
+
+	return true;
+}
+
+pair<int, int> divide_and_conquer(int** nums, int x1, int y1, int x2, int y2) {
+	pair<int, int> res = make_pair(0, 0);
+	if (is_all_same_papers(nums, x1, y1, x2, y2)) {
+		if (nums[x1][y1] == 0) return make_pair(1, 0);
+		else return make_pair(0, 1);
+	}
+
+	int mid_x = (x1 + x2) / 2;
+	int mid_y = (y1 + y2) / 2;
+
+	pair<int, int> tmp;
+	tmp = divide_and_conquer(nums, x1, y1, mid_x, mid_y); // left-top		
+	res.first += tmp.first; res.second += tmp.second;
+	tmp = divide_and_conquer(nums, mid_x + 1, y1, x2, mid_y); // right-top
+	res.first += tmp.first; res.second += tmp.second;
+	tmp = divide_and_conquer(nums, x1, mid_y+1, mid_x, y2); // left-bottom
+	res.first += tmp.first; res.second += tmp.second;
+	tmp = divide_and_conquer(nums, mid_x + 1, mid_y+1, x2, y2); // right-top
+	res.first += tmp.first; res.second += tmp.second;
+
+	return res;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	// Input
+	int N;
+	cin >> N;
+	int i, j;
+	int** nums = (int **)malloc(sizeof(int*) * N);
+	for (i = 0; i < N; i++) {
+		nums[i] = (int*)malloc(sizeof(int)*N);
+		for (j = 0; j < N; j++) cin >> nums[i][j];
+	}
+
+	// Solution
+	pair<int, int> res = divide_and_conquer(nums, 0, 0, N - 1, N - 1);
+	cout << res.first << "\n";
+	cout << res.second << "\n";
+
+	// Free
+	for (i = 0; i < N; i++) {
+		free(nums[i]);
+	} free(nums);
+
+	return 0;
+}
