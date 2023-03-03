0229c5dc4134bbf6c42a695f200c060107da67f4
hanbikan <fchopinof99@gmail.com>
2023-03-03 15:32:25 +0900

    230303

--- Python/test.py
+++ Python/test.py
@@ -2,6 +2,14 @@ from bisect import bisect_left, bisect_right
 from collections import defaultdict, deque
 import sys
 input = sys.stdin.readline
- 
-a,b = [0, [0,0]]
-print(a,b)
+
+
+def get_max_possible(n):
+    cur = 1
+    while cur * 4 <= n:
+        cur *= 4
+    return cur
+
+
+for i in range(1, 100):
+    print(i, get_max_possible(i))
