7cf4305b6f745fcd9cf5ea49ef86d9d231bfa16a
hanbikan <fchopinof99@gmail.com>
2024-04-05 14:23:50 +0900

    240405

--- Python/test.py
+++ Python/test.py
@@ -9,5 +9,3 @@ print(bisect_left([1,2,5,7], 1)) # 0
 print(bisect_left([1,2,5,7], 3)) # 2
 print(bisect_left([1,2,5,7], 9)) # 4
 # N = random.randrange(2, 4)
-
-print(set([1,2,3]) - set([2, 5]))
--- Python/앱.py
+++ Python/앱.py
@@ -1,23 +1,35 @@
+from bisect import bisect_left
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
+
+max_cost = 0
 app = []
 for m, c in zip(input().split(), input().split()):
-    app.append((int(c), int(m)))
-app.sort()
-dp1, dp2 = {0: 0}, {0: 0}
-res = 10**6
+    m, c = int(m), int(c)
+    max_cost += c
+    if c == 0:
+        M -= m
+        N -= 1
+    else:
+        app.append([m, c])
 
-for c, m in app:
-    for key in dp1:
-        new = key + c
-        if new > res:
-            continue
-        if new in dp2:
-            dp2[new] = max(dp1[key]+m, dp2[new])
+dp = [[0] * (max_cost + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    m, c = app[i - 1]
+    for j in range(1, max_cost + 1):
+        if j >= c:
+            dp[i][j] = max(
+                dp[i - 1][j],
+                dp[i - 1][j - c] + m
+            )
         else:
-            dp2[new] = dp1[key] + m
-        if dp2[new] >= M:
-            res = min(res, new)
-    print(dp2)
-    dp1 = dp2.copy()
+            dp[i][j] = dp[i - 1][j]
 
-print(res)
+index = bisect_left(dp[-1], M)
+if dp[-1][index] >= M:
+    print(index)
+else:
+    print(index + 1)
