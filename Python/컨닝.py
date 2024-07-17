f2d8030760b15cb18f1ee80680c2ebfc92add859
hanbikan <fchopinof99@gmail.com>
2024-07-17 18:13:50 +0900

    240717

--- Python/컨닝.py
+++ Python/컨닝.py
@@ -1,144 +1,60 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
+def get_formatted_bit(bit):
+    return bin(bit)[2:][::-1]
 
-def getAvailableDeskCounts():
-    availableDeskCounts = []
-
-    for i in range(M):
-        curCount = 0
-        for j in range(N):
-            if curClassroom[j][i] == '.':
-                curCount += 1
-
-        availableDeskCounts.append(curCount)
-
-    return availableDeskCounts
-
-
-def getSelectionCount(availableDeskCounts):
-    global curClassroom
-    getSelectionCount = 0
-
-    dp = [availableDeskCounts[i] for i in range(M)]
-    dpLog = []
-    dpLog.append(-1)
-    if M >= 2:
-        dpLog.append(-1)
-    if M >= 3:
-        dp[2] += dp[0]
-        dpLog.append(0)
-
-    # dp 및 dpLog 구하기
-    for i in range(3, M):
-        if dp[i-2] > dp[i-3]:
-            dp[i] += dp[i-2]
-            dpLog.append(i-2)
-        else:
-            dp[i] += dp[i-3]
-            dpLog.append(i-3)
-
-    if M == 1:
-        seletedLineIndexes = [0]
-    else:
-        # seletedLineIndexes: 위 dp를 기준으로 최댓값을 얻을 수 있는 라인들 인덱스의 집합
-        if dp[-1] > dp[-2]:
-            seletedLineIndexes = getSeletedLineIndexes(dpLog, M-1)
-        else:
-            seletedLineIndexes = getSeletedLineIndexes(dpLog, M-2)
-
-        # dpTrace의 라인은 모두 학생을 배치한다.
-    for i in seletedLineIndexes:
-        for j in range(N):
-            if curClassroom[j][i] == '.':
-                # y는 해당 좌표에 학생이 배치됐다는 뜻
-                getSelectionCount += 1
-                curClassroom[j][i] = 'y'
-
-    return getSelectionCount
-
-
-# dpLog를 따라가면서 dpTrace를 리턴
-def getSeletedLineIndexes(dpLog, index):
-    seletedLineIndexes = [index]
-
-    curIndex = index
-    while dpLog[curIndex] != -1:
-        seletedLineIndexes.append(dpLog[curIndex])
-        curIndex = dpLog[curIndex]
-
-    return seletedLineIndexes
-
-
-# x, y 좌표에 배치할 수 있는지 확인
-def checkIfAvailablePosition(x, y):
-    global curClassroom
-
-    dx = [0, -1, -1, 0, 1, 1]
-    dy = [-1, -1, 1, 1, -1, 1]
-    for i in range(6):
-        curX, curY = x+dx[i], y+dy[i]
-
-        if 0 <= curX <= N-1 and 0 <= curY <= M-1:
-            if curClassroom[curX][curY] == 'y':
-                return False
-
-    return True
-
-
-def getMaxSelection():
-    global curClassroom
-
-    allAvailablePositions = []
-    for i in range(M):
-        allAvailablePositions += getAvailablePositions(i)
-
-    for i in range(len(allAvailablePositions), 0, -1):
-        curCombinationList = list(combinations(allAvailablePositions, i))
-        for curPositions in curCombinationList:
-            isAvailableClassroom = True
-
-            for pos in curPositions:
-                curClassroom[pos[0]][pos[1]] = 'y'
-                if not checkIfAvailablePosition(pos[0], pos[1]):
-                    isAvailableClassroom = False
-                    break
-
-            if isAvailableClassroom:
-                return i
-
-            for pos in curPositions:
-                curClassroom[pos[0]][pos[1]] = '.'
-
-    return 0
-
-
-def getAvailablePositions(lineIndex):
-    availablePositions = []
-
-    for i in range(N):
-        if curClassroom[i][lineIndex] == '.':
-            if checkIfAvailablePosition(i, lineIndex):
-                availablePositions.append((i, lineIndex))
-
-    return availablePositions
-
-
-N, M = -1, -1
-curClassroom = []
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
-    curClassroom = []
+    mapp = []
     for _ in range(N):
-        curInputLine = input().rstrip()
+        line = list(input().rstrip())
+        mapp.append(line)
 
-        curLine = []
-        for c in curInputLine:
-            curLine.append(c)
+    bits_for_x = [0]
+    for j in range(M):
+        bit_for_x = 0
+        for i in range(N):
+            if mapp[i][j] == 'x':
+                bit_for_x |= 1 << i
+        bits_for_x.append(bit_for_x)
 
-        curClassroom.append(curLine)
+    result = 0
+    dp = [[0]*(2**N) for _ in range(M + 1)]
+    for j in range(M):
+        if j - 1 >= 0:
+            dp[j + 1][0] = result
+        for i in range(N):
+            if mapp[i][j] == 'x':
+                continue
 
-    curAvailableDeskCounts = getAvailableDeskCounts()
-    print(getSelectionCount(curAvailableDeskCounts)+getMaxSelection())
+            for cur_bit in range(1 << i):
+                if cur_bit & bits_for_x[j + 1] != 0:
+                    continue
+                cur_bit_masked = cur_bit | (1 << i)
+
+                cur_bit_1_count = 0
+                test_bit = 0
+                tmp_index = 0
+                tmp_cur_bit = cur_bit_masked
+                while tmp_cur_bit > 0:
+                    if tmp_cur_bit & 1 == 1:
+                        cur_bit_1_count += 1
+
+                        for k in range(tmp_index - 1, tmp_index + 2):
+                            if 0 <= k <= N - 1:
+                                test_bit = test_bit | (1 << k)
+
+                    tmp_index += 1
+                    tmp_cur_bit //= 2
+
+                for prev_bit in range(1 << N):
+                    if prev_bit & test_bit == 0:
+                        dp[j + 1][cur_bit_masked] = max(
+                            dp[j + 1][cur_bit_masked],
+                            dp[j][prev_bit] + cur_bit_1_count,
+                        )
+                        result = max(result, dp[j + 1][cur_bit_masked])
+
+    print(result)
