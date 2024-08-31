8d538a980a473ac2a0b41dcddf087f9e8703509b
hanbikan <fchopinof99@gmail.com>
2024-09-01 02:21:34 +0900

    abc369f

--- Python/abc369f.py
+++ Python/abc369f.py
@@ -11,49 +11,24 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 H, W, N = map(int, input().split())
-has_coin = [[False]*(W+1) for _ in range(H+1)]
-nodes = [set() for _ in range(H+1)]
-for _ in range(N):
-    r, c = map(int, input().split())
-    has_coin[r][c] = True
-    nodes[r].add(c)
+RC = [tuple(map(int, input().split())) for _ in range(N)]
+RC.sort()
 
-dp = [[[0,[]] for _ in range(W+1)] for _ in range(H+1)]
-nodes[1].add(1)
-nodes[H].add(W)
-for i in range(1, H+1):
-    nodes[i].add(1)
-    nodes[i].add(W)
-    if i == 1:
-        for j in range(1, W+1):
-            nodes[i].add(j)
-    else:
-        for j in nodes[i-1]:
-            nodes[i].add(j)
-    nodes[i] = list(nodes[i])
-    nodes[i].sort()
+dp = [(float('inf'), float('inf'))]*N # column 기준 증가하는 수열
+prev = {} # 경로
+for r, c in RC:
+    index = bisect_right(dp, (c, r))
+    dp[index] = (c,r)
+    prev[c,r] = dp[index-1] if index > 0 else (1,1)
 
-    for k in range(len(nodes[i])):
-        j = nodes[i][k]
-        if i == j == 1:
-            continue
+while dp[-1][0] == float('inf'):
+    dp.pop()
+print(len(dp))
 
-        if i >= 2 and j >= 2:
-            left = dp[i][nodes[i][k-1]]
-            up = dp[i-1][nodes[i-1][bisect_left(nodes[i-1], j+1) - 1]]
-            if up[0] > left[0]:
-                dp[i][j] = [up[0], up[1] + ['D']]
-            else:
-                diff = j - nodes[i][k-1]
-                dp[i][j] = [left[0], left[1] + ['R']*diff]
-        elif i == 1: # 윗줄
-            left = dp[i][nodes[i][k-1]]
-            diff = j - nodes[i][k-1]
-            dp[i][j] = [left[0], left[1] + ['R']*diff]
-        else: # 왼쪽
-            dp[i][j] = [dp[i-1][j][0], dp[i-1][j][1] + ['D']]
-        if has_coin[i][j]:
-            dp[i][j][0] += 1
-
-print(dp[-1][-1][0])
-print(''.join(dp[-1][-1][1]))
+path = ['R'*(W - dp[-1][0]), 'D'*(H - dp[-1][1])]
+cur_y, cur_x = dp[-1]
+while (cur_x, cur_y) != (1, 1):
+    prev_y, prev_x = prev[cur_y, cur_x]
+    path.append('R' * (cur_y - prev_y) + 'D' * (cur_x - prev_x))
+    cur_x, cur_y = prev_x, prev_y
+print(''.join(path[::-1]))
