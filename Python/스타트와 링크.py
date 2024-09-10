4af4ba731be3dfee3e883281a1aac5579b9722b6
hanbikan <fchopinof99@gmail.com>
2024-09-10 11:38:35 +0900

    2409010

---
+++ Python/스타트와 링크.py
@@ -0,0 +1,50 @@
+import sys, itertools
+input = sys.stdin.readline
+
+N = int(input())
+S = [list(map(int, input().split())) for _ in range(N)]
+
+all = set(range(N))
+
+cbs = list(itertools.combinations(range(N), N//2))
+result = float('inf')
+for i in range(len(cbs) // 2):
+    a = set(cbs[i])
+    b = all - a
+
+    a_sum = 0
+    for ai in a:
+        for aj in a:
+            a_sum += S[ai][aj]
+
+    b_sum = 0
+    for bi in b:
+        for bj in b:
+            b_sum += S[bi][bj]
+
+    result = min(result, abs(a_sum - b_sum))
+print(result)
+
+'''
+20
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+'''
